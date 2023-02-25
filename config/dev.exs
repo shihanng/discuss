8c1fd8285611675bab0ef9e41e8fa5a6a8902cfb
Shi Han NG <shihanng@gmail.com>
2023-02-25 14:01:42 +0900

    Introduce Tailwind CSS

--- config/dev.exs
+++ config/dev.exs
@@ -27,7 +27,8 @@ config :discuss, DiscussWeb.Endpoint,
   secret_key_base: "7JO45k61dxh4KUoWUuYQgm9jCCLHvlvTGlqlw94M59CDZwDLRbeSb5dPlDCRRHeG",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
