288f37fef0ce5972104110daa688e0a30ab49e7a
Shi Han NG <shihanng@gmail.com>
2023-03-02 07:08:45 +0900

    Fix test environment credential setup

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :discuss, Discuss.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "password",
   hostname: "localhost",
   database: "discuss_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
