66a0e55a77e20813dbfa368fdaa3d488913189d3
Shi Han NG <shihanng@gmail.com>
2023-03-27 21:15:51 +0900

    Add AddUserIdToTopics migration

---
+++ priv/repo/migrations/20230325224513_add_user_id_to_topics.exs
@@ -0,0 +1,9 @@
+defmodule Discuss.Repo.Migrations.AddUserIdToTopics do
+  use Ecto.Migration
+
+  def change do
+    alter table(:topics) do
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+    end
+  end
+end
